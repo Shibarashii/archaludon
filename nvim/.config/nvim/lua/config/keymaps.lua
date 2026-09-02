9dca8d96b9c4d5dfab46e1f1867be3b0b22487c4
Seanrei Ethan M. Valdeabella <147302321+Shibarashii@users.noreply.github.com>
2026-09-02 16:45:15 +0800

    add lualine

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,2 @@
 local map = vim.keymap.set
 
-map('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,8 +1,8 @@
 -- Global tab settings
-vim.opt.tabstop = 4       -- Number of spaces that a <Tab> in the file counts for
-vim.opt.shiftwidth = 4    -- Size of an indent
+vim.opt.tabstop = 2       -- Number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = 2    -- Size of an indent
 vim.opt.expandtab = true  -- Use spaces instead of tabs
-vim.opt.softtabstop = 4   -- Number of spaces a <Tab> feels like when editing
+vim.opt.softtabstop = 2   -- Number of spaces a <Tab> feels like when editing
 vim.opt.number = true
 
 -- Optional: Auto-indent
