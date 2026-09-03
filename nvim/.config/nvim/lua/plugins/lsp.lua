83351eba19fb0467cea9f373206969378bf9be2d
Seanrei Ethan M. Valdeabella <147302321+Shibarashii@users.noreply.github.com>
2026-09-03 15:39:43 +0800

    add mason-tool-installer

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,10 +1,19 @@
 return {
-  "mason-org/mason-lspconfig.nvim",
-  opts = {
-    ensure_installed = { "lua_ls", "pyright", "ruff", "ts_ls" },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = { "lua_ls", "pyright", "ruff", "ts_ls" },
+    },
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
   },
-  dependencies = {
-    { "mason-org/mason.nvim", opts = {} },
-    "neovim/nvim-lspconfig",
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    opts = {
+      ensure_installed = { "stylua", "prettier", "eslint_d", "luacheck" },
+    },
   },
 }
