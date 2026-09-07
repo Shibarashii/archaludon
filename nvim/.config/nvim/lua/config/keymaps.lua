0db6aebb66b8a8db0b313760146f091a2ec4d04b
Seanrei Ethan M. Valdeabella <147302321+Shibarashii@users.noreply.github.com>
2026-09-07 21:47:33 +0800

    add misc plugins: gitsigns, which-key, mini.pairs, add keymap descriptions

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,21 +1,31 @@
 local map = vim.keymap.set
 
+-- Terminal
 map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-map('n', '<C-/>', ':split | term<CR>', { desc = 'Open terminal in split' })
+map('n', '<C-/>', ':split | term<CR>',  { desc = 'Open terminal in split' })
 
+-- Clear search highlight
+map('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlight' })
+
+-- Window navigation
+map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus left' })
+map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus right' })
+map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus down' })
+map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus up' })
 
 vim.api.nvim_create_autocmd('LspAttach', {
   desc = 'LSP actions',
   callback = function(event)
-    local opts = { buffer = event.buf }
-    map('n', 'gd', vim.lsp.buf.definition, opts)
-    map('n', 'gD', vim.lsp.buf.declaration, opts)
-    map('n', 'gi', vim.lsp.buf.implementation, opts)
-    map('n', 'gr', vim.lsp.buf.references, opts)
-    map('n', 'K', vim.lsp.buf.hover, opts)
-    map('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    map({ 'n', 'x' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+    local buf = { buffer = event.buf }
+    map('n', 'gd', vim.lsp.buf.definition,                        vim.tbl_extend('force', buf, { desc = 'Go to definition' }))
+    map('n', 'gD', vim.lsp.buf.declaration,                       vim.tbl_extend('force', buf, { desc = 'Go to declaration' }))
+    map('n', 'gi', vim.lsp.buf.implementation,                    vim.tbl_extend('force', buf, { desc = 'Go to implementation' }))
+    map('n', 'gr', vim.lsp.buf.references,                        vim.tbl_extend('force', buf, { desc = 'Go to references' }))
+    map('n', 'K',  vim.lsp.buf.hover,                             vim.tbl_extend('force', buf, { desc = 'Hover documentation' }))
+    map('n', '<leader>rn', vim.lsp.buf.rename,                    vim.tbl_extend('force', buf, { desc = 'Rename symbol' }))
+    map({ 'n', 'x' }, '<leader>ca', vim.lsp.buf.code_action,     vim.tbl_extend('force', buf, { desc = 'Code action' }))
     -- Formatting is handled by conform.nvim (<leader>gf) with lsp fallback.
     -- Do not add a separate vim.lsp.buf.format binding here.
   end,
 })
+
